2bc811dff64210093b3df4534cab997c607a8e92
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2011-06-19 12:43:51 +0800

    module-starter

---
+++ lib/AnyEvent/Handle/ZeroMQ.pm
@@ -0,0 +1,111 @@
+package AnyEvent::Handle::ZeroMQ;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+AnyEvent::Handle::ZeroMQ - The great new AnyEvent::Handle::ZeroMQ!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use AnyEvent::Handle::ZeroMQ;
+
+    my $foo = AnyEvent::Handle::ZeroMQ->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Cindy Wang (CindyLinz), C<< <cindylinz at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-anyevent-handle-zeromq at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=AnyEvent-Handle-ZeroMQ>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc AnyEvent::Handle::ZeroMQ
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=AnyEvent-Handle-ZeroMQ>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/AnyEvent-Handle-ZeroMQ>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/AnyEvent-Handle-ZeroMQ>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/AnyEvent-Handle-ZeroMQ/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Cindy Wang (CindyLinz).
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of AnyEvent::Handle::ZeroMQ
